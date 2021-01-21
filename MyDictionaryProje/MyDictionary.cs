3fbae991044040d9882b67b3e03e5418807d8d36
furkan paşaoğlu <furkan.pasaoglu@hotmail.com>
2021-01-21 15:43:22 +0300

    Add project files.

---
+++ MyDictionaryProje/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿namespace MyDictionaryProje
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                values[j] = tempValue[j];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionaryProje/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyDictionaryProje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyDictionary
+            MyDictionary<int, string> product = new MyDictionary<int, string>();
+            product.Add(1, "Laptop");
+            product.Add(2, "Mouse");
+            product.Add(3, "Keyboard");
+            product.Add(4, "Camera");
+            product.Add(5, "HeadSet");
+
+            int sayac = 0;
+            foreach (var x in product.Values)
+            {
+                Console.WriteLine(product.Keys[sayac] + " " + x);
+                sayac++;
+            }
+            //Key List
+            Console.WriteLine("\n");
+            Console.WriteLine("KeyList");
+            foreach (var x in product.Keys)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("ValueList");
+            foreach (var x in product.Values)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
